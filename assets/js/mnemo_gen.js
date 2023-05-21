28c0ee3d71a2c08fce2dc9333a3c568deb3f9126
rochersuchard <125407409+rochersuchard@users.noreply.github.com>
2023-05-21 18:20:19 +0200

    Add code

--- assets/js/mnemo_gen.js
+++ assets/js/mnemo_gen.js
@@ -4,6 +4,7 @@ let response = await fetch("assets/js/mnemos.json",{
     }
 });
 let colleges = await response.json();
+
 let sectionSommaire = document.querySelector("#sommaire");
 let sectionMnemo = document.querySelector("#mnemos");
 
@@ -24,22 +25,29 @@ for(let college of colleges)
 
 for(let college of colleges)
 {
+    let collegeElement = document.createElement("div");
+    sectionMnemo.appendChild(collegeElement)
+
     let titreCollege = document.createElement("h2");
     titreCollege.classList.add("titreCollege");
     titreCollege.setAttribute("id", "mm-" + college.nom.toLowerCase());
     let titre = college.nom;
     titreCollege.innerHTML = titre + "&nbsp&nbsp" + college.emoji;
-    sectionMnemo.appendChild(titreCollege);
-
+    collegeElement.appendChild(titreCollege);
     for(let item of (college.contenu))
     {
+        let itemElement = document.createElement("div");
+        collegeElement.appendChild(itemElement);
+
+        itemElement.setAttribute("class", "item-border");
+        
         let titreItem = document.createElement("h3");
         titreItem.innerText = item.titre;
-        sectionMnemo.appendChild(titreItem);
+        itemElement.appendChild(titreItem);
         
         let pElement = document.createElement("p");
         pElement.innerHTML = " " + item.mnemo;
-        sectionMnemo.appendChild(pElement);
+        itemElement.appendChild(pElement);
 
         let ULElement = document.createElement("ul");
         pElement.appendChild(ULElement);
